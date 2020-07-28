7f20514ebd9885fb558047586c07f40dd150637a
Steve Goldstein <sgoldstein@wisc.edu>
2020-07-28 10:59:39 -0500

    Removing path dependencies:  WIP

--- src/simulations_v2/run_sensitivity.sh
+++ src/simulations_v2/run_sensitivity.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 
-python3 run_sensitivity.py $(cat $1 | grep -v \#)  $(echo $@ | awk {'$1 = ""; print $0'})
+exeDir=$(dirname $(readlink -f $0))
+python3 $exeDir/run_sensitivity.py $(cat $1 | grep -v \#)  $(echo $@ | awk {'$1 = ""; print $0'})
